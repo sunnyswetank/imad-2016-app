7c315a03e393682681821c31f01b972f202eb782
sunnyswetank <sunnyswetank@gmail.com>
2016-11-02 22:05:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/test-db',function(req,res){
         if(err){
            res.status(500).send(err.toString());
        } else {
-            res.send(JSON.stringify(result.rows));
+            res.send(JSON.stringify(result.rows[0]));
         }
     });
    //return a response with the results
