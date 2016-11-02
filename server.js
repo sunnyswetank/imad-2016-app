26b25ad810a9073e5182d3d023cc456a56cf2d67
sunnyswetank <sunnyswetank@gmail.com>
2016-11-02 12:33:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = reuiqre('pg').Pool;
+//var Pool = reuiqre('pg').Pool;
 
-var config={
-    user:'sunnyswetank',
-    databse:'sunnyswetank',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD
-};
+//var config={
+//    user:'sunnyswetank',
+//    databse:'sunnyswetank',
+//    host:'db.imad.hasura-app.io',
+//    port:'5432',
+//    password:process.env.DB_PASSWORD
+//};
 var app = express();
 app.use(morgan('combined'));
 
@@ -25,18 +25,18 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-    //make a select statement
-    pool.query('select * from test',function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result));
-        }
-    });
-    //return a response with the results
-});
+//var pool = new Pool(config);
+//app.get('/test-db',function(req,res){
+//    //make a select statement
+//    pool.query('select * from test',function(err,result){
+//        if(err){
+//           res.status(500).send(err.toString());
+//       } else {
+//            res.send(JSON.stringify(result));
+//        }
+//    });
+//   //return a response with the results
+//});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
