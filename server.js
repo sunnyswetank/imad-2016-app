cbd0bb861ef126bdaa11a015d4f2666b8710d8f5
sunnyswetank <sunnyswetank@gmail.com>
2016-11-03 00:01:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var pool = new Pool(config);
-app.get('/test-db',function(req,res){
+app.get('/comments',function(req,res){
     //make a select statement
     pool.query('select commentbody from comments',function(err,result){
         if(err){
@@ -31,21 +31,6 @@ app.get('/test-db',function(req,res){
 
 
 
-//app.get('/#nav6',function(req,res){
-    
-//    pool.query('select commentbody from comments',function(err,result){
-//    if(err){
-//     res.status(500).send(err.toString());   
-//    } else{
-//     if(result.rows.length===0){
-//         res.status(404).send("No comments yet");
-//     } else{
-//         var commentbody=result.rows[0];
-//         res.send(JSON.stringify(result.rows[0]));
-//     }  
-//    }   
-//    });
-//});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
