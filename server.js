7f8c712dc49bbb84202061211963b6bd490ec368
sunnyswetank <sunnyswetank@gmail.com>
2016-11-03 20:16:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,13 +28,34 @@ var commentbody2=c1(data);
         <head>
         </head>
         <body>
- <?php
-  if($_POST['formSubmit'] == "Submit") 
+    <?php
+if($_POST['formSubmit'] == "Submit") 
+{
+  $errorMessage = "";
+ 
+  if(empty($_POST['formMovie'])) 
   {
-    $varMovie = $_POST['formMovie'];
-    $varName = $_POST['formName'];
+    $errorMessage .= "<li>You forgot to enter a movie!</li>";
   }
-?>
+  if(empty($_POST['formName'])) 
+  {
+    $errorMessage .= "<li>You forgot to enter a name!</li>";
+  }
+ 
+  $varMovie = $_POST['formMovie'];
+  $varName = $_POST['formName'];
+ 
+  if(!empty($errorMessage)) 
+  {
+    echo("<p>There was an error with your form:</p>\n");
+    echo("<ul>" . $errorMessage . "</ul>\n");
+  } 
+ 
+}
+    
+?> 
+ 
+ 
 <form action="myform.php" method="post">
     Which is your favorite movie?
     <input type="text" name="formMovie" maxlength="50" value="<?=$varMovie;?>">
