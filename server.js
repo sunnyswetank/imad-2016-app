e8d9b2064e2608e4add17b181cd584fc03311d7d
sunnyswetank <sunnyswetank@gmail.com>
2016-11-03 20:24:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,29 +26,31 @@ var commentbody2=c1(data);
     var htmlTemplate = `
     <html>
         <head>
-        <?php
-if($_POST['formSubmit'] == "Submit") 
-{
-  $errorMessage = "";
-   if(empty($_POST['formMovie'])) 
-  {
-    $errorMessage .= "<li>You forgot to enter a movie!</li>";
-  }
-  if(empty($_POST['formName'])) 
-  {
-    $errorMessage .= "<li>You forgot to enter a name!</li>";
-  }
-  $varMovie = $_POST['formMovie'];
-  $varName = $_POST['formName'];
-  if(!empty($errorMessage)) 
-  {
-    echo("<p>There was an error with your form:</p>\n");
-    echo("<ul>" . $errorMessage . "</ul>\n");
-  } 
- }
-?> 
+
         </head>
         <body>
+        <?php
+            if($_POST['formSubmit'] == "Submit") 
+            {
+              $errorMessage = "";
+               if(empty($_POST['formMovie'])) 
+              {
+                $errorMessage .= "<li>You forgot to enter a movie!</li>";
+              }
+              if(empty($_POST['formName'])) 
+              {
+                $errorMessage .= "<li>You forgot to enter a name!</li>";
+              }
+              $varMovie = $_POST['formMovie'];
+              $varName = $_POST['formName'];
+              if(!empty($errorMessage)) 
+              {
+                echo("<p>There was an error with your form:</p>\n");
+                echo("<ul>" . $errorMessage . "</ul>\n");
+              } 
+             }
+            ?> 
+    
     <form action="myform.php" method="post">
         Which is your favorite movie?
         <input type="text" name="formMovie" maxlength="50" value="<?=$varMovie;?>">
