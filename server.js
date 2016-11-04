f01e128fd8e37161b102d378c5cbf28f64c09816
sunnyswetank <sunnyswetank@gmail.com>
2016-11-04 17:28:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,18 +32,38 @@ var commentbody2=c1(data);
         <body>
          
         <?php
-          if($_POST['formSubmit'] == "Submit") 
+        if($_POST['formSubmit'] == "Submit") 
+        {
+          $errorMessage = "";
+         
+          if(empty($_POST['formMovie'])) 
           {
-            $varMovie = $_POST['formMovie'];
+            $errorMessage .= "<li>You forgot to enter a movie!</li>";
           }
+          if(empty($_POST['formName'])) 
+          {
+            $errorMessage .= "<li>You forgot to enter a name!</li>";
+          }
+         
+          $varMovie = $_POST['formMovie'];
+          $varName = $_POST['formName'];
+         
+          if(!empty($errorMessage)) 
+          {
+            echo("<p>There was an error with your form:</p>\n");
+            echo("<ul>" . $errorMessage . "</ul>\n");
+          } 
+         
+        }
         ?>
         <form action="myform.php" method="post">
-         
             Which is your favorite movie?
-            <input type="text" name="formMovie" maxlength="50" value ="<?php echo $_POST['formMovie'];?>">  
-             
-            <input type="submit" name="formSubmit" value="Submit">
+            <input type="text" name="formMovie" maxlength="50" value="<?=$varMovie;?>">
          
+            What is your name?
+            <input type="text" name="formName" maxlength="50" value="<?=$varName;?>">
+         
+            <input type="submit" name="formSubmit" value="Submit">
         </form>
             <div>
                 <p>${commentbody2}</p>
