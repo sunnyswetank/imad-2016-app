0f9bf0b2fc874817e3d32c91e72471a2406d08f1
sunnyswetank <sunnyswetank@gmail.com>
2016-11-04 17:17:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,22 +30,21 @@ var commentbody2=c1(data);
 
         </head>
         <body>
-      <?php
+         
+        <?php
           if($_POST['formSubmit'] == "Submit") 
           {
             $varMovie = $_POST['formMovie'];
-            $varName = $_POST['formName'];
           }
-        ?>   
-    <form action="myform.php" method="post">
-        Which is your favorite movie?
-        <input type="text" name="formMovie" maxlength="50" value="<?php echo "$varMovie";?>">
-     
-        What is your name?
-        <input type="text" name="formName" maxlength="50" value="<?=$varName?>">
-     
-        <input type="submit" name="formSubmit" value="Submit">
-    </form>
+        ?>
+        <form action="myform.php" method="post">
+         
+            Which is your favorite movie?
+            <input type="text" name="formMovie" maxlength="50">
+             
+            <input type="submit" name="formSubmit" value="Submit">
+         
+        </form>
             <div>
                 <p>${commentbody2}</p>
             </div>
