30270216e665a2f2957a9d1b3a27abf94969cd52
sunnyswetank <sunnyswetank@gmail.com>
2016-11-04 21:09:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,24 +27,12 @@ function c1(input1){
 var commentbody2=c1(data);
     var htmlTemplate = `
     <?php
-    if($_POST['formSubmit'] == "Submit")
-    {
-    	$errorMessage = "";
-    	
-    	if(empty($_POST['formMovie']))
-    	{
-    		$errorMessage .= "<li>You forgot to enter a movie!</li>";
-    	}
-    	if(empty($_POST['formName']))
-    	{
-    		$errorMessage .= "<li>You forgot to enter a name!</li>";
-    	}
-    	
-    	$varMovie = $_POST['formMovie'];
-    	$varName = $_POST['formName'];
+     if($_POST['formSubmit'] == "Submit") 
+      {
+        $varMovie = $_POST['formMovie'];
     }
     ?>
-
+    
     <html>
         <head>
 
