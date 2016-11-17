4021fbf1282a4464665cf876507c3ee0b64225b7
sunnyswetank <sunnyswetank@gmail.com>
2016-11-17 23:11:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,6 +60,19 @@ app.get('/nav6',function(req,res){
     });
 });
 
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
 function hash(input, salt){
 	//how do we create a hash?
 	var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
@@ -109,18 +122,6 @@ app.post('/login',function(req,res){
 			}
 		}
 	});
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
