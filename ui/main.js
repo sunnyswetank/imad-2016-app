c716488e5f865c67984b5d1e42f24691780a45da
sunnyswetank <sunnyswetank@gmail.com>
2016-11-18 00:08:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ submit.onclick=function(){
 	var password = document.getElementById('password').value;
 	console.log(username);
 	console.log(password);
-	request.open('POST','http://sunnyswetank.imad.hasura-app.io/login',true);
+	request.open('POST','http://sunnyswetank.imad.hasura-app.io/login', true);
 	request.setRequestHeader('Content-Type','application/json');
 	request.send(JSON.stringify({username:username,password:password}));
 		
