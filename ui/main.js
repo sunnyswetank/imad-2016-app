d6b9efddc9cdb4bbf111db458feec4c1b8dd5aea
sunnyswetank <sunnyswetank@gmail.com>
2016-11-18 00:36:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,7 @@
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST', '/create-user', true);
+        request.open('POST', 'http://sunnyswetank.imad.hasura-app.io/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         register.value = 'Registering...';
