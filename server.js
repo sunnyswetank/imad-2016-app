83b376fbdf2fd50ffa090d4ab899987670954f61
sunnyswetank <sunnyswetank@gmail.com>
2016-11-18 00:42:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,11 +84,10 @@ function hash(input, salt){
 	return ["pkdbf2","10000",salt, hashed.toString('hex')].join('$');
 }
 
-app.get('/hash/:input',function(req,res){
-	var hashedString = hash(req.params.input, 'this-is-some-random-string');
-	res.send(hashedString);
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
 });
-
 app.post('/create-user', function (req, res) {
    // username, password
    // {"username": "tanmai", "password": "password"}
