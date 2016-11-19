911aba7d4b0dfd115cb7679a38d6af4922ae4250
sunnyswetank <sunnyswetank@gmail.com>
2016-11-19 13:36:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -177,6 +177,24 @@ app.get('/logout', function (req, res) {
 });
 var pool = new Pool(config);
 
+app.post('/submit-comment', function (req, res) {
+   // Check if the user is logged in
+    if (req.session && req.session.auth && req.session.auth.userId) {
+        // First check if the article exists and get the article-id
+        pool.query(
+                "INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)",
+                [req.body.comment, articleId, req.session.auth.userId],
+                function (err, result) {
+                    if (err) {
+                        res.status(500).send(err.toString());
+                        } else {
+                        res.status(200).send('Comment inserted!');
+                        }
+                    });
+    } else {
+    res.status(403).send('Only logged in users can comment');
+    }
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
