97a0f22a063903b5ec496f88f144d5210e4f71ad
sunnyswetank <sunnyswetank@gmail.com>
2016-11-19 15:13:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -88,6 +88,20 @@ function loadLoggedInUser (username) {
     `;
 }
 
+
+function loadCommentForm () {
+    var commentFormHtml = `
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <br/>
+        <input type="submit" id="submit" value="Submit" />
+        <br/>
+        `;
+    document.getElementById('comment_form').innerHTML = commentFormHtml;
+    
+
+}
+
 function loadLogin () {
     // Check if the user is already logged in
     var request = new XMLHttpRequest();
@@ -95,6 +109,7 @@ function loadLogin () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
                 loadLoggedInUser(this.responseText);
+                loadCommentForm(this.responseText);
             } else {
                 loadLoginForm();
             }
