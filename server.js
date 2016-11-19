e5f1e305c3cca610c0f9eab7acf1f0f7fb2f8408
sunnyswetank <sunnyswetank@gmail.com>
2016-11-19 15:40:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+   res.send('http://sunnyswetank.imad.hasura-app.io/#container6');
 });
 var pool = new Pool(config);
 
