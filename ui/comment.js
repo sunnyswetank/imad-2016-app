1c4e7b0aa8d3498a3daec07e7b41ee61e3753aa7
sunnyswetank <sunnyswetank@gmail.com>
2016-11-19 13:39:50 +0530

    [imad-console] Updates ui/comment.js

--- ui/comment.js
+++ ui/comment.js
@@ -31,7 +31,7 @@ function loadCommentForm () {
         
         // Make the request
         var comment = document.getElementById('comment_text').value;
-        request.open('POST', '/nav6', true);
+        request.open('POST', '/submit-comment', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({comment: comment}));  
         submit.value = 'Submitting...';
