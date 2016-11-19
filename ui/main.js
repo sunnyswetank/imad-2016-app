b9abdd3360730ec0c177781bb7c51100ec989242
sunnyswetank <sunnyswetank@gmail.com>
2016-11-19 15:58:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -91,7 +91,6 @@ function loadLoggedInUser (username) {
 
 function loadCommentForm () {
     var commentFormHtml = `
-        <h5>Submit a comment</h5>
         <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
         <br/>
         <input type="submit" id="submit" value="Submit" />
