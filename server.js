6e4ab4eec70f0ceeaed64299c90302eb1c10eb4e
sunnyswetank <sunnyswetank@gmail.com>
2016-11-24 18:38:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -203,6 +203,29 @@ app.get('/get-oldest', function (req, res) {
    });
 });
 
+app.get('/get-latest', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT comment.*,"user".username from comment,"user" where comment.user_id="user".id order by comment.timestamp desc', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
+
+app.get('/get-longest', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT comment.*,"user".username from comment,"user" where comment.user_id="user".id order by comment.timestamp', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
 
 app.post('/submit-comment', function (req, res) {
    // Check if the user is logged in
