882638729a42b7f7e79e3681574bb089f4207491
sunnyswetank <sunnyswetank@gmail.com>
2016-11-25 14:03:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -289,7 +289,7 @@ function loadLongest () {
     };
 }
 
-function loadLike () {
+function loadLike() {
    
     // Submit username/password to login
     var like_btn = document.getElementById('like_btn');
