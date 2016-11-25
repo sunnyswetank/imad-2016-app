ab83536977d5b16e825b7a74e355cd8ccd61f89f
sunnyswetank <sunnyswetank@gmail.com>
2016-11-25 18:24:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -291,6 +291,12 @@ function loadLongest () {
     };
 }
 
+window.onload = function () {
+    document.getElementById('like_btn').onclick= function(){
+        alert('really')
+    };
+};
+
 // The first thing to do is to check if the user is logged in!
 loadLogin();
 loadComments();
