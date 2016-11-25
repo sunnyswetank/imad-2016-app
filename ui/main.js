fb97edd8741a22c3da2d25bd3b6da342c3c93d1a
sunnyswetank <sunnyswetank@gmail.com>
2016-11-25 16:45:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -174,6 +174,7 @@ function loadComments () {
 	                     <button id="dislike_btn" class="w3-btn w3-small w3-round-xlarge w3-light-grey">Dislike()</button>
                         </div>
                         <p>${escapeHTML(commentsData[i].comment)}</p>
+                        <script type="text/javascript" src="ui/comment.js"></script>
                     </div>`;
                 }
                 comments.innerHTML = content;
@@ -289,41 +290,6 @@ function loadLongest () {
     };
 }
 
-function loadLike(){
-window.onload= function() {
-   
-    // Submit username/password to login
-    var like_btn = document.getElementById('like_btn');
-    like_btn.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-                // Take some action
-                if (request.status === 200) {
-                    // clear the form & reload all the comments
-                     var counter=1;
-                } else {
-                    alert('Error! Could not like comment');
-                }
-                like_btn.value = 'Liked';
-          }
-        };
-        
-        // Make the request
-        var like_btn = document.getElementById('like_btn').value;
-        var likes=counter;
-        console.log(counter);
-        request.open('POST', '/submit-like', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({likes: likes}));  
-        like_btn.value = 'Liked';
-        
-    };
-	};
-}
 
 // The first thing to do is to check if the user is logged in!
 loadLogin();
@@ -331,4 +297,4 @@ loadComments();
 loadOldest();
 loadLatest();
 loadLongest();
-loadLike();
+
